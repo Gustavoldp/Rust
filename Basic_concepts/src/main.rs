45b5cfdc103e74909daed941f395b07b8e28e213
Gustavo Lobato de Paulo <88914313+Gustavoldp@users.noreply.github.com>
2023-03-01 23:52:20 -0300

    Basic concepts complete

--- Basic_concepts/src/main.rs
+++ Basic_concepts/src/main.rs
@@ -50,7 +50,83 @@ fn main() {
         Seu tamanho também é fixo, todos os elementos devem ser do mesmo tipo
         e são declarados entre cochetes*/
         let a: [i32; 5] = [1, 2, 3, 4, 5];
+        //Cria um vetor com 5 números 3
+        let b: [i32;5] = [3;5];
+        //Pega o primeiro item do Array
+        let first = a[0];
 
     }
+    //Funções
+    another_function(5);
+    plus_one(4);
 
+    //If Else
+    let condition = true;
+    let number = if condition {5} else {6};
+    println!("The value of number is: {}", number);
+
+    {
+        //Retorno de loop pode vir após o break da condição.
+        let mut counter = 0;
+
+        let result = loop{
+            counter +=1;
+
+            if counter == 10{
+                break counter * 2;
+            }
+        };
+        println!("{}", result);
+    }
+
+    {
+        //um loop com condição pode ser atingido utilizando while
+        let mut number = 3;
+
+        while number != 0 {
+            println!("{}", number);
+
+            number = number - 1;
+        }
+        println!("LIFTOFF!!!");
+    }
+
+    {
+        //Iterando um Array
+        let a = [10, 20, 30, 40, 50];
+        let mut index = 0;
+
+        while index < 5 {
+            println!("{}", a[index]);
+
+            index = index + 1;
+        }
+    }
+
+    {
+        let a = [10, 20, 30, 40, 50];
+
+        for element in a.iter() {
+            println!("{}", element);
+        }
+    }
+
+    {
+        //.rev inverte a ordem
+        for number in (1..4).rev(){
+            println!("{}", number);
+        }
+        println!("LIFTOFF!!!");
+    }
+
+
+
+}
+
+fn another_function(x: i32){
+    println!("The value of x is: {}", x);
+}
+//Retorno implícito, se colocar o ; dps do x+1 da erro, deixa de ser uma expressão
+fn plus_one (x: i32) -> i32{
+    x + 1
 }
