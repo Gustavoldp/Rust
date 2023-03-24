ef4449c0053b24f61df537723ac8290f17625681
Gustavo Lobato de Paulo <88914313+Gustavoldp@users.noreply.github.com>
2023-03-24 14:10:50 -0300

    Enums and Pattern Matching complete

--- Enums_and_Pattern_Matching/src/main.rs
+++ Enums_and_Pattern_Matching/src/main.rs
@@ -34,12 +34,64 @@ enum Coin {
 
 fn main() {
     {
+        //enum options
         let four: IpAddrKind = IpAddrKind::V4(127, 0, 0 ,1);
         let six: IpAddrKind = IpAddrKind::V6(String::from("::1"));
     }
     {
         let m: Message = Message::Write(String::from("hello"));
         m.call();
+    }
+    {
+        //match options
+        let five = Some(5);
+        let six = plus_one(five);
+        let none = plus_one(None);
+    }
+    {
+        //placeholders
+        let some_u8_value: u8 = 0u8;
+        match some_u8_value {
+            1 => println!("one"),
+            3 => println!("three"),
+            5 => println!("five"),
+            7 => println!("seven"),
+            _ => (), //placeholder to catch any other value.
+            
+        }
+    }
+        //if let
+    {
+            let some_u8_value = Some(0u8);
+            match some_u8_value {
+                Some(3) => println!("three"),
+                _ => (),
+            }
+    
+            //this behaves the same as the code above, id let act like a match and uses an = sign to verify the condition
+            if let Some(3) = some_u8_value{
+                println!("three");
+            }
+            //those two does the same thing
+            {
+                fn count_coin(coin: Coin){
+                let mut count = 0;
+                match coin {
+                    Coin::Quarter(state) => print!("State quarter from {:?}", state),
+                    _ => count+=1,
+                }
+            }
+            }
+            {
+                fn count_coin(coin: Coin){
+                let mut count = 0;
+                if let Coin::Quarter(state) = coin {
+                    println!("State quarter from {:?}!", state);
+                }else{
+                    count+1;
+                }
+            }
+            }
     }
 }
 
@@ -52,5 +104,11 @@ fn value_in_cents(coin: Coin) -> u8 {
         println!("State quarter from {:?}", state);
             25
         },
+    }
+}
+fn plus_one(x: Option<i32>) -> Option<i32>{
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
