434dcc9fd5b8b92f0f9c7de7d16e585ce3d83471
Gustavo Lobato de Paulo <88914313+Gustavoldp@users.noreply.github.com>
2023-06-02 20:14:34 -0300

    Writing automated tests

---
+++ writing_automated_tests/adder/target/debug/deps/adder-13749ff49bcf8849.d
@@ -0,0 +1,7 @@
+E:\Gustavo\Extra-curricular\Rust\rust\writing_automated_tests\adder\target\debug\deps\adder-13749ff49bcf8849.rmeta: src\lib.rs
+
+E:\Gustavo\Extra-curricular\Rust\rust\writing_automated_tests\adder\target\debug\deps\libadder-13749ff49bcf8849.rlib: src\lib.rs
+
+E:\Gustavo\Extra-curricular\Rust\rust\writing_automated_tests\adder\target\debug\deps\adder-13749ff49bcf8849.d: src\lib.rs
+
+src\lib.rs:
---
+++ writing_automated_tests/adder/target/debug/deps/adder-65547da1e33be00c.d
@@ -0,0 +1,5 @@
+E:\Gustavo\Extra-curricular\Rust\rust\writing_automated_tests\adder\target\debug\deps\adder-65547da1e33be00c.exe: src\lib.rs
+
+E:\Gustavo\Extra-curricular\Rust\rust\writing_automated_tests\adder\target\debug\deps\adder-65547da1e33be00c.d: src\lib.rs
+
+src\lib.rs:
